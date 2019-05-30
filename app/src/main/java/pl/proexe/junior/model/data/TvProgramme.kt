f94251dd15a03d2f0c441ccfa57121a2cf8b0c70
stoltmanjan <jan.stoltman@proexe.pl>
2019-05-30 17:50:41 +0200

    Create data structures for tv programme and account info

---
+++ app/src/main/java/pl/proexe/junior/model/data/TvProgramme.kt
@@ -0,0 +1,13 @@
+package pl.proexe.junior.model.data
+
+import java.util.*
+
+data class TvProgramme(
+        val title: String,
+        val imageUrl: String,
+        val type: String,
+        val category: TvProgrammeCategory,
+        val isFavourite: Boolean,
+        val startTime: Date,
+        val endTime: Date
+)
