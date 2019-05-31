4d109e988528f260a918989573cef23b3579d93e
stoltmanjan <jan.stoltman@proexe.pl>
2019-05-31 22:18:54 +0200

    Add EpgView

--- app/src/main/java/pl/proexe/junior/view/epg/EpgView.kt
+++ app/src/main/java/pl/proexe/junior/view/epg/EpgView.kt
@@ -1,4 +1,19 @@
 package pl.proexe.junior.view.epg
 
+import pl.proexe.junior.model.data.*
+import java.util.*
+
 interface EpgView {
+
+    fun showEpgList(programmes: List<TvProgramme>)
+
+    fun showDaysList(days: List<DayTile>)
+
+    fun showCategories(categories: List<TvProgrammeCategory>)
+
+    fun showNavigationDrawer(drawerModel: NavigationDrawerModel)
+
+    fun selectDayTile(dayTile: DayTile)
+
+    fun getCurrentlySelectedDateTime(): Date
 }
