faff47bf7973868e5e0456bad7766dd599139f76
stoltmanjan <jan.stoltman@proexe.pl>
2019-06-03 10:24:56 +0200

    Optimize imports

--- app/src/test/java/pl/proexe/junior/model/EpgRepositoryTest.kt
+++ app/src/test/java/pl/proexe/junior/model/EpgRepositoryTest.kt
@@ -1,7 +1,5 @@
 package pl.proexe.junior.model
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
 import pl.proexe.junior.model.repository.epg.EpgRepository
 
 class EpgRepositoryTest {
