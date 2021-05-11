4bf116803bb2d9df08100f1cb674a0f2177c7691
javibanda <72160777+javibanda@users.noreply.github.com>
2021-05-11 15:12:58 +0200

    Add ListTvProgramRecyclerAdapter.kt

---
+++ app/src/main/java/pl/proexe/junior/model/adapter/tvProgram/ListTvProgramRecyclerAdapter.kt
@@ -0,0 +1,21 @@
+package pl.proexe.junior.model.adapter.tvProgram
+
+import android.view.View
+import com.example.proyectofinal.adapter.base.BaseRecyclerAdapter
+import pl.proexe.junior.R
+import pl.proexe.junior.model.data.TvProgramme
+
+class ListTvProgramRecyclerAdapter(
+    private val itemsList: List<TvProgramme>
+): BaseRecyclerAdapter<TvProgramme, ListTvProgramViewHolder>(itemsList){
+
+    override fun createViewHolder(view: View, viewType: Int) = ListTvProgramViewHolder(view)
+
+    override fun getViewHolderLayoutId(viewType: Int) = R.layout.item_tv_program
+
+    override fun onBindViewHolder(holder: ListTvProgramViewHolder, position: Int) {
+       holder.bindListTvProgram(itemsList[position])
+    }
+
+
+}
